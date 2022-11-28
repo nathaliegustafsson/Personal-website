e9d2a1cf18c1be82ad2945febf11c06bfcc9c8ae
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2022-11-28 19:45:52 +0100

    created an animated hamburger icon

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
-let messageArray = ["I'm Nathalie and welcome to my website!"];
-let textPosition = 0;
-let speed = 100;
 
-function typeWriter() {
-    document.querySelector(".welcome-paragraph").
-    innerHTML = messageArray[0].substring(0,
-        textPosition) + "<span>\u25ae</span>";
+// Animation on hamburger-icon
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+// let messageArray = ["I'm Nathalie and welcome to my website!"];
+// let textPosition = 0;
+// let speed = 100;
+
+// function typeWriter() {
+//     document.querySelector(".welcome-paragraph").
+//     innerHTML = messageArray[0].substring(0, 
+//         textPosition) + "<span></span>";
     
-    if(textPosition++ != messageArray[0].length)
-        setTimeout(typeWriter, speed);
-}
+//     if(textPosition++ != messageArray[0].length)
+//         setTimeout(typeWriter, speed);
+// }
 
-window.addEventListener('load', typeWriter);
+// window.addEventListener('load', typeWriter);
+
 
 
