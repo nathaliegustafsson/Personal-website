1837f7d7ef55c978e4c5607d551f08590a5a552c
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2022-12-01 13:13:12 +0100

    David helped me structure & rewrite js code for hamburger and text

--- main.js
+++ main.js
@@ -1,64 +1,44 @@
-// Text typing animation
+window.addEventListener('DOMContentLoaded', main);
 
-let messageArray = ["I'm Nathalie and welcome to my website!"];
-let textPosition = 0;
-let speed = 100;
-
-function typeWriter() {
-    document.querySelector(".welcome-paragraph").
-    innerHTML = messageArray[0].substring(0, 
-        textPosition) + "<span></span>";
-    
-    if(textPosition++ != messageArray[0].length)
-        setTimeout(typeWriter, speed);
+function main() {
+    setupMobileMenu();
+    startTypeWriter();
 }
 
-window.addEventListener('load', typeWriter);
-
-
-// Hamburger andimation and sidebar
-window.onload = function() {
+// Hamburger and sidebar animation
+function setupMobileMenu() {
     const menu_btn = document.querySelector('.hamburger');
     const mobile_menu = document.querySelector('.mobile-nav');
+    const navLinks = document.querySelectorAll('.mobile-nav a');
 
     menu_btn.addEventListener('click', function() {
         menu_btn.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active');
     });
+
+    for (const link of navLinks) {
+        link.addEventListener('click', function() {
+            menu_btn.classList.toggle('is-active');
+            mobile_menu.classList.toggle('is-active');
+        })
+    }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Animation on hamburger-icon
-
-// const menuBtn = document.querySelector('.menu-btn');
-// let menuOpen = false;
-// menuBtn.addEventListener('click', () => {
-//     if (!menuOpen) {
-//         menuBtn.classList.add('open');
-//         menuOpen = true;
-//     } else {
-//         menuBtn.classList.remove('open');
-//         menuOpen = false;
-//     }
-// });
-
-
-
+// Typewriter animation
+function startTypeWriter() {
+    const p = document.querySelector(".welcome-paragraph");
+    const message = "I'm Nathalie and welcome to my website!";
+    const speed = 100;
+    let textPosition = 0;
+    
+    function printNextCharacter() {
+        // Span is the blinking border
+        p.innerHTML = message.substring(0, textPosition) + "<span></span>";
+        
+        if (textPosition != message.length) {
+            setTimeout(printNextCharacter, speed);
+        }
+        textPosition += 1;
+    }
+    printNextCharacter();
+}
